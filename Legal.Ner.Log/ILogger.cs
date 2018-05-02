384d8973f31fa6d5caf0d019e64e1e41dc444132
Juan Fernando Rojas Moreno <egeda9@gmail.com>
2018-05-02 09:25:13 -0500

    Add Legal.Ner.Log project

---
+++ Legal.Ner.Log/ILogger.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Legal.Ner.Log
+{
+    public interface ILogger
+    {
+        void Info(string message);
+        void Error(string message, Exception exception);
+    }
+}
---
+++ Legal.Ner.Log/Implementations/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+using log4net;
+
+namespace Legal.Ner.Log.Implementations
+{
+    public class Logger : ILogger
+    {
+        private readonly ILog _log;
+
+        public Logger()
+        {
+            _log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        }
+
+        public void Info(string message)
+        {
+            _log.Info(message);
+        }
+
+        public void Error(string message, Exception exception)
+        {
+            _log.Error(message, exception);
+        }
+    }
+}
